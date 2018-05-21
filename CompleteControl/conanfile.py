b33fe8d72c421bf248936138d1eef4831c8ce028
Unknown <drmcmurderface2@mail.com>
2018-05-21 15:38:10 -0500

    qwer

--- CompleteControl/conanfile.py
+++ CompleteControl/conanfile.py
@@ -1,8 +1,8 @@
 from conans import ConanFile
 
 class TM_HelloWorld_Conan(ConanFile):
-    requires="OBSEPluginDevPackage/0.1@Troy1010/channel"
+    requires="OBSEPluginDevPackage/0.1@Troy1010/channel","TM_CommonCPP/0.1@Troy1010/channel"
     generators="visual_studio"
 
     import HookBuildInfo as HBI
-    HBI.HookBuildInfo('obse_plugin_example.vcxproj','conanbuildinfo.props')
+    HBI.HookBuildInfo('CompleteControl.vcxproj','conanbuildinfo.props')
