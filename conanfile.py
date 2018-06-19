d1a44748de6d5430fd6aaf0eb1cd054405d8fadf
Unknown <drmcmurderface2@mail.com>
2018-06-19 14:09:43 -0500

    conanfile

--- conanfile.py
+++ conanfile.py
@@ -6,24 +6,27 @@ sProj = "CompleteControl/CompleteControl.vcxproj"
 from conans import ConanFile
 import xml.etree.ElementTree
 import os, sys, ctypes
-import VisualStudioAutomation as VS
 import TM_CommonPy as TM
+import VisualStudioAutomation as VS
+import pickle
+import dill
 ##endregion
 ##region DoubleclickEvent
 if __name__ == "__main__":
     try:
         TM.Run("conan install . -pr conanprofile_OBSEPlugin")
-        VS.SetTMDefaultSettings(sProj)
-        VS.IntegrateProps(sProj,"conanbuildinfo.props")
-        ##region Integrate OBSEPluginDevPackage RecommendedIntegrationFiles
-        with TM.ElementTreeContext("conanbuildinfo.props") as vTree:
-            sOBSEPluginDevPackageRoot = TM.FindElem(xml.etree.ElementTree.Element("Conan-OBSEPluginDevPackage-Root"),vTree).text
-        sys.path.insert(0,sOBSEPluginDevPackageRoot)
-        import RecommendedIntegration
-        RecommendedIntegration.Main(os.path.abspath(sProj),sOBSEPluginDevPackageRoot)
-        ##endregion
-    except:
-        ctypes.windll.user32.MessageBoxW(0, "Exception occured.", __file__, 1)
+        vCommandSet = TM.CommandSet.TryLoad()
+        vCommandSet.Que((VS.SetTMDefaultVSSettings.Do,VS.SetTMDefaultVSSettings.Undo),sProj)
+        vCommandSet.Que([VS.IntegrateProps,VS.IntegrateProps_Undo],[sProj,"conanbuildinfo.props"])
+        vCommandSet.QueConanPackageRecommendedIntegrations(sProj,"conanbuildinfo.txt")
+        vCommandSet.Execute()
+        vCommandSet.Save()
+    except Exception as e:
+        if bPause:
+            print(e)
+            os.system('pause')
+        else:
+            ctypes.windll.user32.MessageBoxW(0, "Exception occured.", __file__, 1)
         raise
     if bPause:
         print("\n\t\t\tDone\n")
