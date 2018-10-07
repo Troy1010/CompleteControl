bc658f4de56956fedaa3a6f7f94140dedbe33d63
Unknown <drmcmurderface2@mail.com>
2018-10-06 23:11:36 -0500

    conanfile

--- conanfile.py
+++ conanfile.py
@@ -1,29 +1,4 @@
-##region Settings
-bPause = True
-sProj = "CompleteControl/CompleteControl.vcxproj"
-sSln = "CompleteControl.sln"
-##endregion
-##region Imports
 from conans import ConanFile
-import os, sys
-import TM_CommonPy as TM
-import traceback
-##endregion
-##region DoubleclickEvent
-if __name__ == "__main__":
-    try:
-        TM.Run("conan install . -pr conanprofile_OBSEPlugin")
-    except Exception as e:
-        print("====================================================================")
-        print("Traceback (most recent call last):")
-        traceback.print_tb(e.__traceback__)
-        print(e)
-        os.system('pause')
-        sys.exit(1)
-    if bPause:
-        print("\n\t\t\tDone\n")
-        os.system('pause')
-##endregion
 
 class CompleteControl_Conan(ConanFile):
     name="CompleteControl"
