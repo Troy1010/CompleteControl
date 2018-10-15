1db31ace3b0215a3f24ae9acc85271d479112cc0
Unknown <drmcmurderface2@mail.com>
2018-10-15 12:24:53 -0500

    cleanup

--- CompleteControl/src/main.cpp
+++ CompleteControl/src/main.cpp
@@ -423,8 +423,6 @@ bool OBSEPlugin_Query(const OBSEInterface * obse, PluginInfo * info)
 #endif	
 	}
 
-	_MESSAGE("OBSE version too old (got %08X expected at least %08X)", obse->obseVersion, OBSE_VERSION_INTEGER);
-	_MESSAGE("OBSE version too old (got %i expected at least %i)", obse->obseVersion, OBSE_VERSION_INTEGER);
 	if (obse->obseVersion < OBSE_VERSION_INTEGER)
 	{
 		_MESSAGE("OBSE version too old (got %08X expected at least %08X)", obse->obseVersion, OBSE_VERSION_INTEGER);
