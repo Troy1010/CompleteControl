7e3ef3a45e99ceb73b19a56ae73f516318cbb029
Unknown <drmcmurderface2@mail.com>
2018-11-10 12:58:25 -0600

    Debug-now removes all % to prevent format error. Needs update for new TM

--- CompleteControl/include/CompleteControl/DebugCC.h
+++ CompleteControl/include/CompleteControl/DebugCC.h
@@ -2,6 +2,7 @@
 #include "Misc.h"
 //#include "Globals.h"
 #include "Settings.h"
+#include "TM_CommonCPP/String.h"
 
 extern IDebugLog		gLog; //Log
 void FnDebug(std::string sString);
--- CompleteControl/src/DebugCC.cpp
+++ CompleteControl/src/DebugCC.cpp
@@ -5,5 +5,5 @@ IDebugLog		gLog(DebugLogName); //Log
 void FnDebug(std::string sString)
 {
 	SafeConsolePrint(sString);
-	_MESSAGE(sString.c_str());
+	_MESSAGE(TMC::Str::Replace(sString,"%","").c_str());
 }
