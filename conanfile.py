76bb5fd5c016290797e4aff1a1c21fd95bfd3124
Unknown <drmcmurderface2@mail.com>
2019-01-07 10:01:57 -0600

    requires TM_CommonCPP beta, not beta_source

--- conanfile.py
+++ conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile
 
 class CompleteControl_Conan(ConanFile):
     name = "CompleteControl"
-    requires = "OBSEPluginDevPackage/0.1@Troy1010/channel", "TM_CommonCPP/0.1@Troy1010/beta_source"
+    requires = "OBSEPluginDevPackage/0.1@Troy1010/channel", "TM_CommonCPP/0.1@Troy1010/beta"
     generators = "visual_studio"
     license = "MIT"
     description = "OBSE plugin to resolve EnableKey conflicts between mods."
